67232e4f589e4c6dc88cd03395ff338bd8f3fdf2
PaulOrtizCastro <97984808+PaulOrtizCastro@users.noreply.github.com>
2022-03-21 11:25:55 -0500

    Add files via upload

---
+++ Searchfight_v03/Searchfight/Servicios/BuscarServicio.cs
@@ -0,0 +1,44 @@
+﻿using Searchfight.Motores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searchfight.Servicios
+{
+    public abstract class BuscarServicio
+    {
+        public abstract string Name { get; }
+        public int WinnerResults = 0;
+        private string Winner = "No hay resultados";
+
+        public abstract IMotorBusqueda CreateEngine();
+        public string GetResults(string word)
+        {
+            IMotorBusqueda engine = CreateEngine();
+
+            int results = engine.Search(word);
+
+            SetWinner(word, results);
+
+            return $"{Name} : {results}";
+        }
+        public string WinnerToString()
+        {
+            return $"{Name} winner : {Winner}";
+        }
+        private void SetWinner(string word, int results)
+        {
+            if (results > WinnerResults)
+            {
+                Winner = word;
+                WinnerResults = results;
+            }
+        }
+        public string GetWinner()
+        {
+            return Winner;
+        }
+    }
+}
---
+++ Searchfight_v03/Searchfight/Servicios/GoogleService.cs
@@ -0,0 +1,18 @@
+﻿using Searchfight.Motores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searchfight.Servicios
+{
+    public class GoogleService: BuscarServicio
+    {
+        public override string Name => "Google";
+        public override IMotorBusqueda CreateEngine()
+        {
+            return new Google();
+        }
+    }
+}
