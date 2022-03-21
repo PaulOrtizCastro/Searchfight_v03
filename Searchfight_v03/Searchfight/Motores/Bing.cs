d2609fbce76d5968e4da77769a68186c69d1032f
PaulOrtizCastro <97984808+PaulOrtizCastro@users.noreply.github.com>
2022-03-21 11:23:31 -0500

    Add files via upload

---
+++ Searchfight_v03/Searchfight/Motores/Bing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searchfight.Motores
+{
+    public class Bing : Base, IMotorBusqueda
+    {
+        public int Search(string word)
+        {
+            string key = obtenerLlave("bingkey");
+            string url = "https://api.cognitive.microsoft.com/bingcustomsearch/v7.0/search" + "?q=" + word + "&customconfig=0";
+            AddHeader("Ocp-Apim-Subscription-Key", key);
+            var result = GetResponse(url);
+            return Convert.ToInt32(result["webPages"]["totalEstimatedMatches"]);
+        }
+    }
+}
---
+++ Searchfight_v03/Searchfight/Motores/Google.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searchfight.Motores
+{
+    public class Google : Base, IMotorBusqueda
+    {
+        public int Search(string word)
+        {
+            string key = obtenerLlave("googlekey");
+            string url = "https://www.googleapis.com/customsearch/v1?key=" + key + "&q=" + word;
+            var result = GetResponse(url);
+            return Convert.ToInt32(result["queries"]["request"][0]["totalResults"]);
+        }        
+    }
+}
