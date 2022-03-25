8d9522d2ff87e67c80ce7b997381d1c23bc39480
Fausto Ortiz Castro <97984808+PaulOrtizCastro@users.noreply.github.com>
2022-03-25 13:11:57 -0500

    Add files via upload

---
+++ Searchfight_v04/Searchfight/Servicios/BingService.cs
@@ -0,0 +1,18 @@
+﻿using Searchfight.Motores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searchfight.Servicios
+{
+    class BingService: BuscarServicio
+    {
+        public override string Name => "Bing";
+        public override IMotorBusqueda CreateEngine()
+        {
+            return new Bing();
+        }
+    }
+}
