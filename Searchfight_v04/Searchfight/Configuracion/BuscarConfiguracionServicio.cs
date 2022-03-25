ac7cb7dc43e0e9450770cd625241231b0ab1a3d5
Fausto Ortiz Castro <97984808+PaulOrtizCastro@users.noreply.github.com>
2022-03-25 13:11:03 -0500

    Add files via upload

---
+++ Searchfight_v04/Searchfight/Configuracion/BuscarConfiguracionServicio.cs
@@ -0,0 +1,32 @@
+﻿using Searchfight.Factory;
+using Searchfight.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searchfight.Configuracion
+{
+    class BuscarConfiguracionServicio : ISearchServiceFactory
+    {
+        public string obtenerGanador(BuscarServicio[] services)
+        {
+            List<string> winners = new List<string>();
+
+            foreach (BuscarServicio service in services)
+            {
+                winners.Add(service.GetWinner());
+            }
+            return winners[1];
+        }
+
+        public BuscarServicio[] obtenerServicios()
+        {
+            return new BuscarServicio[] {
+                new GoogleService(),
+                new BingService()
+            };
+        }
+    }
+}
---
+++ Searchfight_v04/Searchfight/Motores/Bing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searchfight.Motores
+{
+    public class Bing : Base, IMotorBusqueda
+    {
+        public int Search(string word)
+        {
+            string key = obtenerLlave("bingkey");
+            string url = "https://api.cognitive.microsoft.com/bingcustomsearch/v7.0/search" + "?q=" + word + "&customconfig=0";
+            AddHeader("Ocp-Apim-Subscription-Key", key);
+            var result = GetResponse(url);
+            int rpt = 0;
+            try
+            {
+                if (result != null) {
+                    rpt = Convert.ToInt32(result["webPages"]["totalEstimatedMatches"]);
+                }
+                
+            }
+            catch (Exception e) {
+                rpt = 0;
+            }
+            return rpt;
+        }
+    }
+}
