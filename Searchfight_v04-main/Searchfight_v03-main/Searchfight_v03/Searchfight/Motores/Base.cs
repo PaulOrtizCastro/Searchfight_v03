1dd6fa170cdf31b4262f40322e03d8d48acdc8bf
Fausto Ortiz Castro <97984808+PaulOrtizCastro@users.noreply.github.com>
2022-03-25 13:04:48 -0500

    Add files via upload

---
+++ Searchfight_v04-main/Searchfight_v03-main/Searchfight_v03/Searchfight/Motores/Base.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Searchfight.Motores
+{
+    public class Base
+    {
+        private readonly HttpClient _httpClient;
+        public Base()
+        {
+            _httpClient = new HttpClient();
+        }
+        public JObject GetResponse(string url)
+        {
+            try
+            {
+                _httpClient.Timeout = TimeSpan.FromSeconds(3);
+                var data = _httpClient.GetStringAsync(url).Result;
+                return JObject.Parse(data);
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.Message);
+                return null;
+                //throw;
+            }
+        }
+        public void AddHeader(string key, string value)
+        {
+            _httpClient.DefaultRequestHeaders.Add(key, value);
+        }
+        public string obtenerLlave(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
